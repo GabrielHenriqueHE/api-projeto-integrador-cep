f81c018ed651ea8e66e68deb8bd534894d1a3eb1
GabrielHenriqueHE <gabriel.evaristo.2004@gmail.com>
2022-06-17 18:56:38 -0300

    Revert "Update database.ts"
    This reverts commit 86cc07e000e426749136ff21156347864557b352.

--- app/bin/config/database.ts
+++ app/bin/config/database.ts
@@ -1,11 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const PORT = Number(process.env.PORT) || 3306;
-
-const sequelizeConnection = new Sequelize('sql10500217', 'sql10500217', 'V6hRgMh88u', {
-    host: 'sql10.freemysqlhosting.net',
-    dialect: 'mysql',
-    port: PORT
+const sequelizeConnection = new Sequelize('sistemaDeNotasFiscais', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'mysql'
 });
 
 sequelizeConnection.authenticate().then(() => {
