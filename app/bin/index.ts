749fb26a268810c72d01c1b536050388be2c6abb
GabrielHenriqueHE <gabriel.evaristo.2004@gmail.com>
2022-06-17 17:58:36 -0300

    Configurando yarn start

--- app/bin/index.ts
+++ app/bin/index.ts
@@ -1,6 +1,10 @@
 import http from "http";
 import app from "../src";
 
+import dotenv from "dotenv";
+
+dotenv.config()
+
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 5500;
