1e5ab18124532e00114371851e1a8c673b4fcfde
GabrielHenriqueHE <gabriel.evaristo.2004@gmail.com>
2022-06-17 16:24:18 -0300

    Atualização de HTTP GET
    - HTTP GET para obter as empresas e as notas fiscais.

---
+++ app/src/entities/useCases/ReturnCompaniesUseCase/ReturnCompaniesUseCase.ts
@@ -0,0 +1,11 @@
+import Empresa from "../../Empresa";
+
+export async function ReturnCompaniesUseCase(): Promise<any> {
+
+    const companies = await Empresa.findAll({  })
+
+    if (companies.length >= 1) {
+        return companies;
+    } else return false;
+    
+}
--- app/src/routes.ts
+++ app/src/routes.ts
@@ -1,17 +1,23 @@
 import { Router } from "express";
 
 import index from "./controllers";
-import CreateCompanyController from "./controllers/CreateCompanyController";
-import CreateInvoiceController from "./controllers/CreateInvoiceController";
+
 import { CreateCompanyMiddleware } from "./middlewares/CreateCompanyMiddleware";
 import { CreateInvoiceMiddleware } from "./middlewares/CreateInvoiceMiddleware";
+
+import CreateCompanyController from "./controllers/CreateCompanyController";
+import CreateInvoiceController from "./controllers/CreateInvoiceController";
+import { ReturnCompaniesController } from "./controllers/ReturnCompaniesController";
+import { ReturnInvoicesController } from "./controllers/ReturnInvoicesController";
 
 const router = Router();
 
 router.get("/", index);
 
+router.get("/empresas", ReturnCompaniesController);
 router.post("/empresas/nova", CreateCompanyMiddleware, CreateCompanyController);
 
+router.get("/notas", ReturnInvoicesController);
 router.post("/notas/nova", CreateInvoiceMiddleware, CreateInvoiceController);
 
 export default router;
